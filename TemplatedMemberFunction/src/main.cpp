28eddc79874ab098d813dbba192757de226370e9
chadd williams <will4614@pacificu.edu>
2023-04-06 14:53:15 -0700

    working multiproject

--- TemplatedMemberFunction/src/main.cpp
+++ TemplatedMemberFunction/src/main.cpp
@@ -11,6 +11,7 @@
 #include <vector> 
 #include <string>
 #include <sstream>
+#include "DataReader.h"
 
 //***************************************************************************
 // Function:    main
@@ -23,7 +24,25 @@
 //***************************************************************************
 int main()
 {
-    std::string cMessage = "hi!";
-    std::cout << ">> " << cMessage << " << " << std::endl;  
-    return EXIT_SUCCESS;
+  DataReader cReader("data/values.txt");
+
+  int intValue;
+  float floatValue;
+  std::string strValue;
+
+  cReader.readData(intValue);
+  std::cout << intValue << " " ;
+  cReader.readData(strValue);
+  std::cout << strValue << " " ;
+
+  cReader.readData(floatValue);
+  std::cout << floatValue << " " ;
+
+  if (cReader.readData(intValue))
+  {
+    std::cout << intValue;
+  }
+
+  std::cout << std::endl;
+
 }
